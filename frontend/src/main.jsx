7fd3488299361ee39670437d67317fc8b046a2df
Shubham <shubhambhutada7810@gmail.com>
2024-06-12 19:57:36 +0530

    Created StoreContext and also made the ExploreMenu component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { StoreContextProvider } from "./context/StoreContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <React.StrictMode>
+    <StoreContextProvider>
       <App />
-    </React.StrictMode>
+    </StoreContextProvider>
   </Router>
 );
