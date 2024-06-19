326cfda0fc9d484faa46f7d8330eaffc7946e191
Shubham <shubhambhutada7810@gmail.com>
2024-06-20 00:36:40 +0530

    Created Admin panel page and made Add Food component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,13 +5,16 @@ import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
-      <div className="add-content">
+      <div className="app-content">
         <Sidebar />
         <Routes>
           <Route path="/add" element={<Add />} />
