b60f99702724c1882ca5dcf65ac3615539f28855
Shubham <shubhambhutada7810@gmail.com>
2024-06-24 16:39:32 +0530

    Created remove from cart api

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,7 +3,7 @@ const userModel = require("../models/userModel.js");
 //add items to user cart
 const addToCart = async (req, res) => {
   try {
-    let userData = await userModel.findOne({ _id: req.body.userId });
+    let userData = await userModel.findById(req.body.userId);
     let cartData = await userData.cartData;
     if (!cartData[req.body.itemId]) {
       cartData[req.body.itemId] = 1;
@@ -25,7 +25,26 @@ const addToCart = async (req, res) => {
 };
 
 //remove items from user cart
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({
+      success: true,
+      message: "Removed from Cart",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 
 //fetch user cart data
 const getCart = async (req, res) => {};
